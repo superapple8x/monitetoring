26a9f06c588d09d3c27f1445a1ff54c38df7fb4b
superapple8x <superapple223@gmail.com>
2025-06-05 07:54:34 +0700

    Add Security Alerts and Historical Charts components to the dashboard

--- moninet-dashboard/src/App.tsx
+++ moninet-dashboard/src/App.tsx
@@ -3,6 +3,8 @@ import DashboardLayout from "./components/Layout/DashboardLayout";
 import DeviceList from "./components/DeviceList/DeviceList";
 import BandwidthUsage from "./components/BandwidthUsage/BandwidthUsage";
 import ConnectivityStatus from "./components/ConnectivityStatus/ConnectivityStatus";
+import HistoricalCharts from "./components/HistoricalCharts"; // Added import
+import SecurityAlerts from "./components/SecurityAlerts"; // Added import
 import { websocketService } from "./services/websocketService";
 
 function App() {
@@ -22,8 +24,12 @@ function App() {
   return (
     <DashboardLayout>
       <div className="space-y-6">
-        <ConnectivityStatus />
-        <BandwidthUsage />
+        <SecurityAlerts />
+        <HistoricalCharts />
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6"> {/* Grouping older components */}
+          <ConnectivityStatus />
+          <BandwidthUsage />
+        </div>
         <DeviceList />
       </div>
     </DashboardLayout>
